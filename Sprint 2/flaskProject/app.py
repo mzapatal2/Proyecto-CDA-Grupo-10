a31e84aa8d948563b5a54c4bbf1887c54a99e003
mzapatal2 <m.zapatal2@uniandes.edu.co>
2021-10-19 22:57:39 -0500

    Inicio Pagina web

---
+++ Sprint 2/flaskProject/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def index():  # put application's code here
+    nombre = 'Nombre'
+    return render_template('index.html', nombre=nombre)
+
+
+@app.route('/test')
+def hello_world1():  # put application's code here
+    return 'project'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
