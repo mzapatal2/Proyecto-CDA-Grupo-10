77aaeae6bb6b90490c5a8d896fff988c8d4f843f
nirobe <n.rozo10@uniandes.edu.co>
2021-11-13 13:45:01 -0500

    actualizacion de archivos

--- Sprint 2/flaskProject/app.py
+++ Sprint 2/flaskProject/app.py
@@ -8,13 +8,17 @@ CORS(app)
 
 @app.route('/')
 def index():  # put application's code here
-    nombre = 'Nombre'
+    nombre = ''
     return render_template('index.html', nombre=nombre)
 
 
-@app.route('/test')
-def hello_world1():  # put application's code here
-    return 'projectyyyyyyy'
+@app.route('/CalcularModelo1', methods = ['POST', 'GET'])
+def CalcularModelo1():  # put application's code here
+    tipoRequest = request.method
+    nombre = 'Mateo'
+    value = request.args.get('idParMetodo1')
+    result = request.form
+    return render_template('index.html', nombre=value)
 
 
 if __name__ == '__main__':
